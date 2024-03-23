7b60ef370645d128a363feaeb5d8ee4c616e89f9
Jarrod <144126463+1994TROY@users.noreply.github.com>
2024-03-23 11:10:14 +0200

    Added my Header and Navbar

---
+++ RVNBEATS Podcast/src/components/layout/Header.jsx
@@ -0,0 +1,26 @@
+// Header.jsx
+import React, { useState } from 'react';
+import './Header.css';
+import NavBar from './NavBar';
+
+const Header = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <header className="app-header">
+      <button
+        className={`toggle-button ${isSidebarOpen ? 'open' : ''}`}
+        onClick={toggleSidebar}
+      >
+        {isSidebarOpen ? 'X' : '☰'} {}
+      </button>
+      <NavBar isOpen={isSidebarOpen} />
+    </header>
+  );
+};
+
+export default Header;
